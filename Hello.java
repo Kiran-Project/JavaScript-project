0615594f65d5e356069563979dbc820048660000
Kiran-Project <143247967+Kiran-Project@users.noreply.github.com>
2023-08-26 11:52:28 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+class Hello extends Frame
+  { 
+     public static void main(String [] args)
+    { 
+      Hello h1=new Hello();
+      h1.setVisible(true);
+      h1.setSize(500,500);
+    }
+  }
